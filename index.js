56bddfef1436f92197209c9aa10fa0e6a636bb2a
ariksfaradi <ariksfaradi@users.noreply.github.com>
2017-09-22 23:48:55 +0300

    change

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send({ hi: 'there' }));
+app.get('/', (req, res) => res.send({ hi: 'you there' }));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`server is listening on port ${PORT}`));
+
+// Creating app... done, ⬢ enigmatic-hollows-66515
+// https://enigmatic-hollows-66515.herokuapp.com///
+// | https://git.heroku.com/enigmatic-hollows-66515.git
